203ad86c270baee24bececf8adf90cf77bf8308d
Nicolas <mymobile.f430@gmail.com>
2021-10-20 13:55:15 +0200

    03 metodo getter dinero acumulado

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -36,6 +36,13 @@ public class MaquinaExpendedoraSimple {
     }
     
     /**
+     * Devuelve el dinero acumulado
+     */
+    public int getTotalDineroAcumulado(){
+        return totalDineroAcumulado;
+    }
+    
+    /**
      * Devuelve el precio del billete
      */
     public int getPrecioBillete() {
